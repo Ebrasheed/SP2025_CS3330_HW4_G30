9421f91b6dfb0d0db3c0e524eef87d94ed6c89df
ebras <ebras@HooverVacuum>
2025-04-22 16:15:28 -0500

    added the import for arraylist

--- SP2025_CS3330_HW4_G30/src/hw4/maze/Row.java
+++ SP2025_CS3330_HW4_G30/src/hw4/maze/Row.java
@@ -1,5 +1,37 @@
 package hw4.maze;
 
+/**
+ * Represents a row in the maze, consisting of multiple cells.
+ */
 public class Row {
+    private ArrayList<Cell> cells;
 
+    public Row() {
+        this.cells = new ArrayList<>();
+    }
+
+    public void addCell(Cell cell) {
+        this.cells.add(cell);
+    }
+
+    public Cell getCell(int index) {
+        return cells.get(index);
+    }
+
+    public void setCell(int index, Cell cell) {
+        this.cells.set(index, cell);
+    }
+
+    public int getCellsSize() {
+        return cells.size();
+    }
+
+    public ArrayList<Cell> getCells() {
+        return cells;
+    }
+
+    @Override
+    public String toString() {
+        return cells.toString();
+    }
 }
