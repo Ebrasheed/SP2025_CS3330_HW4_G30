16f73c42ad72b2c33eeda47d9da40c1268e8be09
ebras <ebras@HooverVacuum>
2025-04-22 16:38:10 -0500

    added simulation to make sure everything works

--- SP2025_CS3330_HW4_G30/src/hw4/game/Simulation.java
+++ SP2025_CS3330_HW4_G30/src/hw4/game/Simulation.java
@@ -1,5 +1,29 @@
 package hw4.game;
 
-public class Simulation {
+import hw4.maze.*;
+import hw4.player.Player;
 
+/**
+ * Simulation class to run the game.
+ */
+public class Simulation {
+    public static void main(String[] args) {
+        Game game = new Game();
+        Grid grid = game.getGrid();
+        int size = grid.getGridSize();
+
+        // Start the player at the bottom-right
+        Player player = new Player(size - 1, size - 1);
+
+        // Display initial grid state
+        System.out.println("Initial Player Position: " + player);
+        System.out.println("Grid:");
+        System.out.println(grid);
+
+        player.moveUp();
+        System.out.println("After moving up: " + player);
+
+        player.moveLeft();
+        System.out.println("After moving left: " + player);
+    }
 }
